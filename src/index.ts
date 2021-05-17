09402061157920bbe6eb12c4a6a0ca7ddc69192a
James Kerber <kerber.jg@gmail.com>
2021-05-17 15:15:01 +0200

    Removed RestImplementationError

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { HttpRequest } from './request';
 import { HttpRouter, HttpRoute, CrudAdapter, CrudResponse, HttpMethod, HttpRouteCallback, HttpRouteFormattingError } from './routes';
 import { HttpServerService, HttpServerConfig } from './httpServer.service';
 import { HttpApiLayer, HttpApiConfig } from './httpApi.layer';
-import { RestImplementationError } from './errors/restImplementation.error';
 import { HttpError, HttpErrorType } from './errors/http.error';
 
 export {
@@ -10,5 +9,5 @@ export {
     HttpServerConfig, HttpApiConfig,
     HttpRequest,
     HttpMethod, HttpRouteCallback, HttpRouter, HttpRoute, CrudAdapter, CrudResponse,
-    HttpRouteFormattingError, RestImplementationError, HttpError, HttpErrorType
+    HttpRouteFormattingError, HttpError, HttpErrorType
 };

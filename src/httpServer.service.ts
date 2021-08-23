7eb8d74c5e0fb132af6137372d1a6569584b76ba
James Kerber <kerber.jg@gmail.com>
2021-08-23 16:26:43 +0200

    Implemented body parsing

--- src/httpServer.service.ts
+++ src/httpServer.service.ts
@@ -4,6 +4,7 @@ import { HttpRequest } from './request';
 
 import { Express as ExpressApp } from 'express';
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
 
 import * as colors from 'colors/safe';
 import { Server } from 'node:http';
@@ -59,6 +60,9 @@ export class HttpServerService extends Service {
         const app: ExpressApp = express();
         // Attach middleware
         require('express-async-errors');
+        app.use(bodyParser.json());
+        app.use(bodyParser.text());
+        app.use(bodyParser.urlencoded());
 
         // Apply settings
         for (const key in this.config.expressSettings) {
--- src/routes.ts
+++ src/routes.ts
@@ -73,7 +73,7 @@ export abstract class HttpRoute {
         // PUT (update)
         this.route('/:id').put(async (event) => {
             await new Promise(resolve => Express.raw()(event.data.req, event.data.res, resolve));
-            const res = await model.update(event.data.req.params.id, event.data.req.query as any);
+            const res = await model.update(event.data.req.params.id, event.data.req.body);
             event.data.res.status(res.code).send(res.data);
         });
 
